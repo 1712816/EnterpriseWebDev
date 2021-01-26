c7a1053f1089742a266beba7dc0fcfc789746460
corsejack <34166649+1712816@users.noreply.github.com>
2021-01-26 07:43:50 +0000

    Update main_html.js

--- main_html.js
+++ main_html.js
@@ -1,14 +1,10 @@
-var http = require("http");
+const http = require('http')
+const fs = require('fs')
 
-http.createServer(function (request, response) {
-   // Send the HTTP header
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('EnterpriseWebSys.html').pipe(res)
+})
 
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8000);
-
-// Console will print the message
+server.listen(process.env.PORT || 8000)
 console.log('Server running at http://127.0.0.1:8000/');
